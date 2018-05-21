b2d168445c79fced5b06c7c107b71f4b9f4e0ab0
befern <belen.fdez.glez@gmail.com>
2018-05-21 23:43:30 +0200

    Passport

--- models/user.js
+++ models/user.js
@@ -1,20 +1,14 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMogoose = require('passport-local-mongoose');
 
 var User = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
+User.plugin(passportLocalMogoose);
 
 module.exports = mongoose.model('User', User);
